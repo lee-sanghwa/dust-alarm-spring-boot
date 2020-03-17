c4229c1e4b734a3bb8c5e68f8603bcff34f75644
이상화 <joseph@devhi.me>
2020-03-17 21:59:18 +0900

    외부와 통신을 하는 코드가 중복되어 함수처리

--- src/main/java/com/dustalarm/common/DustAlarmCommon.java
+++ src/main/java/com/dustalarm/common/DustAlarmCommon.java
@@ -1,16 +1,19 @@
-package com.dustalarm.rest;
+package com.dustalarm.common;
 
 import com.dustalarm.model.Alarm;
 import com.dustalarm.model.User;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Collection;
+import java.util.Map;
 
 public class DustAlarmCommon {
-
-    public static User setUserAgent(User target, String userAgent) {
-        target.setVersion(userAgent);
-        return target;
-    }
 
     public static Alarm setUserAgent(Alarm target, String userAgent) {
         target.setVersion(userAgent);
@@ -29,5 +32,29 @@ public class DustAlarmCommon {
             target.setVersion(userAgent);
         }
         return targets;
+    }
+
+    public static JsonNode getResponseBodyFromUrl(String url, Map<String, String> headerConfig) throws IOException {
+        URL urlObject = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) urlObject.openConnection();
+
+        /* Using Lambda For setting connection header */
+        headerConfig.forEach(connection::setRequestProperty);
+        connection.connect();
+
+        /* get Json Format From Body */
+        BufferedReader tempBr = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+        StringBuilder sb = new StringBuilder();
+        String tempLine;
+        while ((tempLine = tempBr.readLine()) != null) {
+            sb.append(tempLine + "\n");
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode responseBody = mapper.readTree(sb.toString());
+        tempBr.close();
+        connection.disconnect();
+
+        return responseBody;
     }
 }
